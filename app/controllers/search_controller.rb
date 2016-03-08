a75ee1038c09ca84201e8cb399bb246c17c45465
Hector Huertas <hectorhuertas@gmail.com>
2016-03-08 11:01:42 -0700

    WIP figuring out best buy api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     @conn = Faraday.new(url: 'https://api.bestbuy.com/v1', params: {apiKey: ENV['BEST_BUY_API_KEY']})
-    b = @conn.get('/products')
+    b = @conn.get('/products(manufacturer-cannon)')
     json = JSON.parse b.body
     binding.pry
   end
