005533115f76a426c451a6de7e6b78099ecc9928
Hector Huertas <hectorhuertas@gmail.com>
2016-03-08 09:45:38 -0700

    Improve endpoint testing

--- test/integration/items_api_test.rb
+++ test/integration/items_api_test.rb
@@ -6,11 +6,27 @@ class ProjectSetupTest < ActionDispatch::IntegrationTest
     Capybara.app = Storedom::Application
   end
 
+  def create_items(number)
+    number.times do |i|
+      Item.create(
+      name: "Item_#{i}",
+      description: "Item_description_#{i}",
+      image_url: "Image_url_#{i}"
+      )
+    end
+  end
+
+  def json
+    JSON.parse(response.body)
+  end
+
 
   test "items index endpoint" do
+    create_items(2)
+
     get "/api/v1/items"
+    assert response.success?
     assert_equal response.status, 200
-    binding.pry
-    assert_equal response.lenght, 500
+    assert_equal json['items'].size, 2
   end
 end
